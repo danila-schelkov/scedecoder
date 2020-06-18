065ab37f24df0ec98303174ab2e9a9988361504d
Данила Щелков (Danila Schelkov) <54549682+Vorono4ka@users.noreply.github.com>
2020-06-18 22:09:19 +0400

    Scripts Uploaded

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+
+from utils.reader import Reader
+import json
+
+
+class SCEDecode(Reader):
+    def __init__(self, file_name: str, encrypted: bool = True):
+        try:
+            file_path = ('encrypted/' if encrypted else 'decrypted/') + file_name
+            filedata = open(file_path, 'rb').read()
+            decrypted = self.decrypt(filedata, 'secrets.')
+            open(f'decrypted/{file_name}.decrypted.data', 'wb').write(decrypted)
+
+            super().__init__(decrypted)
+            items = []
+            while len(decrypted) > 0:
+                parsed, length = self.parse()
+                if length == 0:
+                    break
+                items.append(parsed)
+
+            output = json.dumps(items, indent=4)
+            open(f'parsed/{file_name}.parsed.json', 'w').write(output)
+        except Exception as e:
+            print(e)
+
+    def parse(self):
+        length = self.readInteger()
+        items = []
+        i = 0
+        while i < length / 2:
+            items.append({
+                'key': self.readString(),
+                'value': self.readString()
+            })
+            i += 1
+        return items, length
+
+    def decrypt(self, data, key):
+        result = b''
+        c = 0
+        while c < len(data):
+            charCode = data[c] ^ ord(key[c % len(key)])
+            result += charCode.to_bytes(1, 'big')
+            c += 1
+        return result
+
+
+if __name__ == '__main__':
+    if not os.path.exists('decrypted'):
+        os.mkdir('decrypted')
+    if not os.path.exists('encrypted'):
+        os.mkdir('encrypted')
+    if not os.path.exists('parsed'):
+        os.mkdir('parsed')
+    sce = SCEDecode('log_current.sce')
